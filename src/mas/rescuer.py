a359df7e0b706e1e87098ac7fe0ac241177261c4
hyon.mota <hyonboklucas@gmail.com>
2025-07-10 09:56:54 -0300

    Mudanças gap rescuer

--- src/mas/rescuer.py
+++ src/mas/rescuer.py
@@ -330,10 +330,10 @@ class Rescuer(AbstAgent):
         for vic_id in sequence:
             goal = self.victims[vic_id][0]
             plan = aStar.search(start, goal)
-            time = plan[len(plan)- 1][1] * 1.25 + 1 # Assume que vai perder mais tempo do que o planejado
+            time = plan[len(plan)- 1][1] * 1.35 + 1 # Assume que vai perder mais tempo do que o planejado
             base_plan = aStar.search(goal, base)
-            base_time = base_plan[len(base_plan) - 1][1] * 2 # Garante o dobro de tempo de volta
-            if(self.plan_rtime - time < base_time + 75): # +75 de gap inicial
+            base_time = base_plan[len(base_plan) - 1][1] * 1.35 # Garante tempo de volta
+            if(self.plan_rtime - time < base_time + 5): # +5 de gap inicial
                 self.victims_left.append(vic_id)
                 continue
 
@@ -408,7 +408,7 @@ class Rescuer(AbstAgent):
 
             # Se sobrou algum, avisa na explicabilidade
             for victim in total_left:
-                print(f"Vítima {victim} não será socorrida pelo devido ao tempo escasso restante.")
+                print(f"Vítima {victim} não será socorrida devido ao tempo escasso restante.")
                 print(self.explain_victim_decision(victim))
 
     def setup_rescuer(self, index):
